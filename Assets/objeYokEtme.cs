8d12fba92acd3ee69c507278b9d43ffa1d90ca87
Burak Hammusoglu <m.burakhammusoglu@gmail.com>
2022-02-14 00:40:51 +0300

    Destroy işlemleri,obje silme,component silme

---
+++ Assets/objeYokEtme.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objeYokEtme : MonoBehaviour
+{
+    void Start()
+    {
+        //destroy ile istediğimiz objeyi yok ederiz.
+        //Destroy(gameObject);
+
+        //belirli bi süre sonra yok etmek istiyorsak
+        //Destroy(gameObject,2f);//virgül atıp süreyi yazarız 2sn geçtikten sonra yok edecektir.
+        
+        // peki şu şekilde yazarsak
+        //Destroy(this); // script dosyasını siler.
+        
+        //Objeye ait componenti silmek istersek;
+        Destroy(GetComponent<BoxCollider>());
+    }
+
+    
+}
